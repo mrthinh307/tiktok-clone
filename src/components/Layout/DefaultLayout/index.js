b11df9eed4d5798a104db856033abd0edb29654d
mrthinh307 <23020708@vnu.edu.vn>
2025-04-14 18:50:15 +0700

    feat: implement DefaultLayout and SideBar components with basic layout styling

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,10 +1,16 @@
+import classNames from 'classnames/bind';
+import styles from './DefaultLayout.module.scss';
 import SideBar from './SideBar';
+
+const cx = classNames.bind(styles);
 
 function DefaultLayout({ children }) {
     return (
-        <div>
-            <SideBar />
-            <div className="content">{children}</div>
+        <div className={cx('wrapper')}>
+            <div className={cx('container')}>
+                <SideBar />
+                <div className={cx('content')}>{children}</div>
+            </div>
         </div>
     );
 }
