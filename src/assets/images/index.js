8b82c4166e66fc3e6834bddc175890250c7263e3
mrthinh307 <23020708@vnu.edu.vn>
2025-04-16 10:40:36 +0700

    feat: add dark logo SVG and implement Header component with search functionality

---
+++ src/assets/images/index.js
@@ -0,0 +1,5 @@
+const images = {
+    darkLogo: require('./dark-logo.svg').default,
+}
+
+export default images;
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,14 +1,40 @@
 import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faCircleXmark,
+    faMagnifyingGlass,
+    faSpinner,
+} from '@fortawesome/free-solid-svg-icons';
+
 import styles from './Header.module.scss';
+import images from '~/assets/images/index';
 
 const cx = classNames.bind(styles);
 
 function Header() {
-    return <header className={cx('wrapper')}>
-        <div className={cx('inner-content')}>
-            
-        </div>
-    </header>;
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner-content')}>
+                <div className={cx('logo')}>
+                    <img src={images.darkLogo} alt="logo" />
+                </div>
+                <div className={cx('search')}>
+                    <input placeholder="Seach" spellCheck={false} />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon
+                        className={cx('loading')}
+                        icon={faSpinner}
+                    />
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('action')}></div>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
