86f3a28122248bb5339990021ddf303374dad0d1
mrthinh307 <23020708@vnu.edu.vn>
2025-04-22 10:05:00 +0700

    refactor: update Sidebar component to use props for active state and click handling; enhance Setting component with smooth scrolling and content section styling

--- src/pages/Settings/Sidebar.js
+++ src/pages/Settings/Sidebar.js
@@ -1,26 +1,21 @@
 import classNames from 'classnames/bind';
 import styles from './Setting.module.scss';
-import { useState } from 'react';
 import MenuItem from '~/components/Popper/Menu/MenuItem';
 import { SETTING_SIDEBAR_ITEMS, SETTING_SIDEBAR_ITEM_PROPS } from './constants';
 
 const cx = classNames.bind(styles);
 
-function Sidebar() {
-    const [activeSidebar, setActiveSidebar] = useState(0);
-
+function Sidebar({ activeSidebar, onItemClick }) {
     const renderSidebars = (items) => {
-        return (
-            items.map((item, index) => (
-                <MenuItem
-                    key={index}
-                    data={item}
-                    active={activeSidebar === index}
-                    onClick={() => setActiveSidebar(index)}
-                    {...SETTING_SIDEBAR_ITEM_PROPS}
-                />
-            ))
-        )
+        return items.map((item, index) => (
+            <MenuItem
+                key={index}
+                data={item}
+                active={activeSidebar === index}
+                onClick={() => onItemClick(index)}
+                {...SETTING_SIDEBAR_ITEM_PROPS}
+            />
+        ));
     };
 
     return (
--- src/pages/Settings/index.js
+++ src/pages/Settings/index.js
@@ -1,27 +1,51 @@
 import classNames from 'classnames/bind';
 import styles from './Setting.module.scss';
 import ContentItem from './ContentItem';
+import { useState, useRef } from 'react';
 import { ArrowBackIcon } from '~/assets/images/icons';
 import { SETTING_CONTENT_ITEMS } from './constants';
 import Sidebar from './Sidebar';
 
 const cx = classNames.bind(styles);
 
-
-
-const renderContents = (items) => {
-    return items.map((item, index) => {
-        return <ContentItem key={index} data={item} />;
-    });
-};
-
 function Setting() {
+    const [activeSidebar, setActiveSidebar] = useState(0);
+
+    const contentRefs = useRef([]);
+
+    const handleSidebarClick = (index) => {
+        setActiveSidebar(index);
+
+        if (contentRefs.current[index]) {
+            contentRefs.current[index].scrollIntoView({
+                behavior: 'smooth',
+                block: 'start',
+            });
+        }
+    };
+
+    const renderContents = (items) => {
+        return items.map((item, index) => (
+            <div
+                key={index}
+                ref={(el) => (contentRefs.current[index] = el)}
+                className={cx('content-section')}
+            >
+                <ContentItem data={item} />
+            </div>
+        ));
+    };
+
     return (
         <div className={cx('setting-wrapper')}>
             <div className={cx('setting-container')}>
                 <ArrowBackIcon className={cx('back-button')} />
                 <div className={cx('setting-main')}>
-                    <Sidebar />
+                    <Sidebar
+                        activeSidebar={activeSidebar}
+                        onItemClick={handleSidebarClick}
+                    />
+
                     <div className={cx('setting-content', 'hide-scrollbar')}>
                         {renderContents(SETTING_CONTENT_ITEMS)}
                     </div>
