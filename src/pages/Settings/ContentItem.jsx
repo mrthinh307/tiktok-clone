66bb2359163e3bd03996a550d7be18f53396cc65
mrthinh307 <23020708@vnu.edu.vn>
2025-04-23 10:15:47 +0700

    refactor: restructure ContentItem component and enhance PropTypes validation for better readability and type safety

--- src/pages/Settings/ContentItem.jsx
+++ src/pages/Settings/ContentItem.jsx
@@ -5,110 +5,140 @@ import Tippy from '@tippyjs/react';
 
 const cx = classNames.bind(styles);
 
-function ContentItem({ data }) {
-    const renderChildren = (children) => {
-        if (!children) return null;
+// Component tooltip
+const ContentTooltip = ({ tooltip }) => (
+    <Tippy
+        content={<span className={cx('tooltip-box')}>{tooltip.desc}</span>}
+        maxWidth="none"
+        placement="top"
+        delay={[100, 200]}
+    >
+        <div className={cx('tooltip-icon')}>{tooltip.icon}</div>
+    </Tippy>
+);
 
-        return children.map((child, index) => (
-            <div key={index} className={cx('content-group')}>
-                {child.icon && (
-                    <div className={cx('content-icon')}>{child.icon}</div>
-                )}
-                <div>
-                    <div
-                        className={cx('content-text', {
-                            'strong-text': child.strongTitle,
-                        })}
-                    >
-                        {child.title}
-                    </div>
-                    <div className={cx('content-desc')}>{child.desc}</div>
-                </div>
-                {child.action && (
-                    <div
-                        className={cx(
-                            'content-action',
-                            `${child.action.type}-action`,
-                        )}
-                        onClick={child.action.onClick}
-                    >
-                        {!!child.action.text && (
-                            <span className={cx('text-action')}>
-                                {child.action.text}
-                            </span>
-                        )}
-                        {child.action.content}
-                    </div>
-                )}
-            </div>
-        ));
-    };
+// Component Action (shared between item and child)
+const ContentAction = ({ action }) => (
+    <div
+        className={cx('content-action', `${action.type}-action`)}
+        onClick={action.onClick}
+    >
+        {!!action.text && (
+            <span className={cx('text-action')}>{action.text}</span>
+        )}
+        {action.content}
+    </div>
+);
 
-    const renderItems = (items) => {
-        return items.map((item, index) => (
+// Component for subtitle container
+const ContentSubtitle = ({ item }) => (
+    <div className={cx('subtitle-container')}>
+        <div className={cx('content-subtitle')}>{item.title}</div>
+        {item.desc && item.desc.length > 0 && (
+            <div className={cx('content-desc')}>{item.desc}</div>
+        )}
+        {item.action && <ContentAction action={item.action} />}
+    </div>
+);
+
+// Component for each child item of ContentItemSingle
+const ContentChild = ({ child }) => (
+    <div className={cx('content-group')}>
+        {child.icon && <div className={cx('content-icon')}>{child.icon}</div>}
+        <div>
             <div
-                key={index}
-                className={cx('content-item', { 'no-title': !item.title })}
+                className={cx('content-text', {
+                    'strong-text': child.strongTitle,
+                })}
             >
-                {item.title && (
-                    <div className={cx('subtitle-container')}>
-                        <div className={cx('content-subtitle')}>
-                            {item.title}
-                        </div>
-                        {item.desc && item.desc.length > 0 && (
-                            <div className={cx('content-desc')}>
-                                {item.desc}
-                            </div>
-                        )}
-                        {item.action && (
-                            <div
-                                className={cx(
-                                    'content-action',
-                                    `${item.action.type}-action`,
-                                )}
-                                onClick={item.action.onClick}
-                            >
-                                {!!item.action.text && (
-                                    <span className={cx('text-action')}>
-                                        {item.action.text}
-                                    </span>
-                                )}
-                                {item.action.content}
-                            </div>
-                        )}
-                    </div>
-                )}
-                {item.children && renderChildren(item.children)}
+                {child.title}
             </div>
-        ));
-    };
+            <div className={cx('content-desc')}>{child.desc}</div>
+        </div>
+        {child.action && <ContentAction action={child.action} />}
+    </div>
+);
 
+// Component for each content item
+const ContentItemSingle = ({ item }) => (
+    <div className={cx('content-item', { 'no-title': !item.title })}>
+        {item.title && <ContentSubtitle item={item} />}
+        {item.children &&
+            item.children.map((child, index) => (
+                <ContentChild key={index} child={child} />
+            ))}
+    </div>
+);
+
+// main Component
+function ContentItem({ data }) {
     return (
         <div className={cx('content-wrapper')}>
             <div className={cx('content-container')}>
                 <div className={cx('content-title')}>
                     {data.title}
-                    {data.tooltip && (
-                        <Tippy
-                            content={
-                                <span className={cx('tooltip-box')}>
-                                    {data.tooltip.desc}
-                                </span>
-                            }
-                            maxWidth="none"
-                        >
-                            <div className={cx('tooltip-icon')}>
-                                {data.tooltip.icon}
-                            </div>
-                        </Tippy>
-                    )}
+                    {data.tooltip && <ContentTooltip tooltip={data.tooltip} />}
                 </div>
-                {renderItems(data.items)}
+
+                {data.items &&
+                    data.items.map((item, index) => (
+                        <ContentItemSingle key={index} item={item} />
+                    ))}
             </div>
         </div>
     );
 }
 
+// PropTypes for each component
+ContentTooltip.propTypes = {
+    tooltip: PropTypes.shape({
+        desc: PropTypes.string,
+        icon: PropTypes.node,
+    }).isRequired,
+};
+
+ContentAction.propTypes = {
+    action: PropTypes.shape({
+        type: PropTypes.string,
+        text: PropTypes.string,
+        onClick: PropTypes.func,
+        content: PropTypes.node,
+    }).isRequired,
+};
+
+ContentChild.propTypes = {
+    child: PropTypes.shape({
+        icon: PropTypes.node,
+        title: PropTypes.string,
+        desc: PropTypes.string,
+        strongTitle: PropTypes.bool,
+        action: PropTypes.shape({
+            type: PropTypes.string,
+            text: PropTypes.string,
+            onClick: PropTypes.func,
+            content: PropTypes.node,
+        }),
+    }).isRequired,
+};
+
+ContentSubtitle.propTypes = {
+    item: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        desc: PropTypes.string,
+        action: PropTypes.object,
+    }).isRequired,
+};
+
+ContentItemSingle.propTypes = {
+    item: PropTypes.shape({
+        title: PropTypes.string,
+        desc: PropTypes.string,
+        action: PropTypes.object,
+        children: PropTypes.array,
+    }).isRequired,
+};
+
+// PropTypes for ContentItem component
 ContentItem.propTypes = {
     data: PropTypes.shape({
         title: PropTypes.string.isRequired,
@@ -116,32 +146,7 @@ ContentItem.propTypes = {
             desc: PropTypes.string,
             icon: PropTypes.node,
         }),
-        items: PropTypes.arrayOf(
-            PropTypes.shape({
-                title: PropTypes.string,
-                desc: PropTypes.string,
-                action: PropTypes.shape({
-                    type: PropTypes.string,
-                    text: PropTypes.string,
-                    onClick: PropTypes.func,
-                    content: PropTypes.node,
-                }),
-                children: PropTypes.arrayOf(
-                    PropTypes.shape({
-                        icon: PropTypes.node,
-                        title: PropTypes.string,
-                        desc: PropTypes.string,
-                        strongTitle: PropTypes.bool,
-                        action: PropTypes.shape({
-                            type: PropTypes.string,
-                            text: PropTypes.string,
-                            onClick: PropTypes.func,
-                            content: PropTypes.node,
-                        }),
-                    }),
-                ),
-            }),
-        ),
+        items: PropTypes.array,
     }).isRequired,
 };
 
