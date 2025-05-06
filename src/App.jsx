ef130ae498625fbb78003914b8118f56019631db
mrthinh307 <23020708@vnu.edu.vn>
2025-05-06 23:02:26 +0700

    feat: integrate Tailwind CSS for styling and update project dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import './styles/tailwind.css'
 import { Fragment } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { publicRoutes } from './routes';
--- src/pages/Following/index.js
+++ src/pages/Following/index.js
@@ -1,5 +1,16 @@
 function Following() {
-    return <h2>Following Page</h2>;
+    return (
+        <div className="bg-white p-6 rounded-2xl shadow-md hover:shadow-xl transition duration-300 max-w-md mx-auto">
+            <h2 className="text-2xl font-semibold text-gray-800 mb-2">
+                🎉 Tính năng mới
+            </h2>
+            <p className="text-gray-600">
+                Component này đang sử dụng{' '}
+                <span className="font-medium text-blue-600">Tailwind CSS</span>{' '}
+                để tạo UI đẹp và nhanh.
+            </p>
+        </div>
+    );
 }
 
 export default Following;
