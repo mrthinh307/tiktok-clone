32c1132a40b54c6f100a9b5f4787390dc0fec901
mrthinh307 <23020708@vnu.edu.vn>
2025-05-19 14:30:23 +0700

    Refactor boilerplate structure
    - remove Babel and ESLint configurations, update environment variables, and migrate to TypeScript with a new server setup
    - add package-lock.json and tsconfig.json for dependency management and TypeScript configuration.

--- src/utils/sorts.ts
+++ src/utils/sorts.ts
@@ -1,11 +1,4 @@
 /**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- * ---
- * Order an array of objects based on another array & return new Ordered Array
- * The original array will not be modified.
- * ---
  * @param {*} originalArray
  * @param {*} orderArray
  * @param {*} key = Key to order
@@ -15,7 +8,7 @@
  * Xác định các phần tử trong array gốc ban đầu (originalArray) xem nó nằm ở đâu trong array thứ 2 (orderArray) (là array mà mình dùng để sắp xếp) bằng cách tìm index (indexOf) rồi sẽ sắp xếp theo index đó bằng hàm sort của Javascript.
  */
 
-export const mapOrder = (originalArray, orderArray, key) => {
+export const mapOrder = (originalArray: any[], orderArray: any[], key: any) => {
   if (!originalArray || !orderArray || !key) return []
 
   const clonedArray = [...originalArray]
