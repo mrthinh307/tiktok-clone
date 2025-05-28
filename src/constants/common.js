d3183f302a758d9d0240ea57c10f9c756bb90d02
mrthinh307 <23020708@vnu.edu.vn>
2025-05-28 23:11:43 +0700

    refactor: update default avatar URL and add fade-in animations for sidebar components

--- src/constants/common.js
+++ src/constants/common.js
@@ -1,2 +1,3 @@
 export const DEBOUNCE_DELAY = 500; // 500ms
-export const DEFAULT_AVATAR = 'https://www.svgrepo.com/show/341256/user-avatar-filled.svg';
+export const DEFAULT_AVATAR = 'https://i.pinimg.com/736x/9f/16/72/9f1672710cba6bcb0dfd93201c6d4c00.jpg';
+export const DEFAULT_AVATAR_URL_API = 'https://files.fullstack.edu.vn/f8-tiktok/';
