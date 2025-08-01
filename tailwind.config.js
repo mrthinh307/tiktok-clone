fb26354cf0b6fa52b477059398c696206eb8a597
mrthinh307 <23020708@vnu.edu.vn>
2025-08-01 15:04:51 +0700

    fix(DefaultLayout): adjust content width and remove fixed margin for better responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,8 @@ module.exports = {
     extend: {
       colors: {
         primary: '#fe2c55',
+        body: '#161823',
+        muted: '#161823BF',
       },
       keyframes: {
         shimmer: {
@@ -15,11 +17,30 @@ module.exports = {
       animation: {
         shimmer: 'shimmer 2s infinite',
       },
+      fontFamily: {
+        regular: 'var(--font-family-regular)',
+        medium: 'var(--font-family-medium)',
+        boldfont: 'var(--font-family-bold)',
+      }
     },
   },
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
     require('@tailwindcss/aspect-ratio'),
+    function({ addUtilities }) {
+      addUtilities({
+        '.scrollbar-hide': {
+          /* IE and Edge */
+          '-ms-overflow-style': 'none',
+          /* Firefox */
+          'scrollbar-width': 'none',
+          /* Safari and Chrome */
+          '&::-webkit-scrollbar': {
+            display: 'none'
+          }
+        }
+      })
+    }
   ],
 };
